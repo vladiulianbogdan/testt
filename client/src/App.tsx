c0f949276e78bf0dd5f06758f98e41718de0f75d
Bogdan Vlad <vladbgd@gmail.com>
2024-08-07 15:51:05 +0300

    Create some errors.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/React.svg";
 import { BackendService } from "@genezio-sdk/genezio-project";
-import "./App.css";
+import "./app.css";
 
 export default function App() {
   const [name, setName] = useState("");
